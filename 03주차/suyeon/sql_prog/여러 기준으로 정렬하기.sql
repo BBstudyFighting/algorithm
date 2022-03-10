1a20cf07e5e8fd066b7e7c80c13f7129a5a325f6
ssu-yeon <98367433+ssu-yeon@users.noreply.github.com>
2022-03-10 23:35:00 +0900

    Update 여러 기준으로 정렬하기.sql

--- 03주차/suyeon/sql_prog/여러 기준으로 정렬하기.sql
+++ 03주차/suyeon/sql_prog/여러 기준으로 정렬하기.sql
@@ -1,3 +1,5 @@
 SELECT ANIMAL_ID, name, DATETIME
 from ANIMAL_INS
-order by name asc, datetime desc
+order by name asc, datetime desc 
+# 단 이름이 같은 동물 중에서는 보호를 먼저 시작한 동물을 먼저 보여달라고 하니
+# name으로 한번 오름차순 정렬하고 그 다음에 datetime으로 내림차순 정렬을 해야 된다는 말이다
