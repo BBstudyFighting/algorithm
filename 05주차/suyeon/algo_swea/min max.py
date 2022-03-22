b106f19fcaae5310f94b110a5154fbde3afa7988
ssu-yeon <98367433+ssu-yeon@users.noreply.github.com>
2022-03-22 17:44:46 +0900

    Update min max.py

--- 05주차/suyeon/algo_swea/min max.py
+++ 05주차/suyeon/algo_swea/min max.py
@@ -27,10 +27,8 @@ T = int(input()) # 테스트 갯수(3)를 input하고 int로 변경
 
 for tc in range(1, T+1): # range(1,4): 1이상 4미만
     N = int(input()) # nums의 총 갯수 
-    nums = list(map(int, input().split())) 
-    # nums에 포함된 숫자들을 input하고 공백 기준으로 나누어 int로 바꾸고 리스트형식으로 변경
-    answer = max(nums) - min(nums)
-		# 문제에서 가장 큰 수와 가장 작은 수의 차이를 물어봤기 때문에 max(nums) - min(nums)로 작성
+    nums = list(map(int, input().split()))  # nums에 포함된 숫자들을 input하고 공백 기준으로 나누어 int로 바꾸고 리스트형식으로 변경
+    answer = max(nums) - min(nums)  # 문제에서 가장 큰 수와 가장 작은 수의 차이를 물어봤기 때문에 max(nums) - min(nums)로 작성
 
     # 최종 출력 예시
     print('#{} {}'.format(tc, answer))
