79edf0e71340bd3830bfc034a2f53f215c5fc2f6
ssu-yeon <98367433+ssu-yeon@users.noreply.github.com>
2022-03-27 23:20:52 +0900

    Create 베스트앨범.py

---
+++ 05주차/suyeon/algo_pro/베스트앨범.py
@@ -0,0 +1,73 @@
+'''
+스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 
+노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
+1. 속한 노래가 많이 재생된 장르를 먼저 수록합니다.
+2. 장르 내에서 많이 재생된 노래를 먼저 수록합니다.
+3. 장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
+노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 
+베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
+
+'''
+
+def solution(genres, plays):
+    answer = []
+    dic = {}
+    for i in range(len(genres)):
+        if genres[i] in dic.keys():
+            dic[genres[i]] = dic[genres[i]] + [i]
+        else:
+            dic[genres[i]] = [i]
+            
+    dic_sort = sorted(dic, key=lambda x: sum(plays[i] for i in dic[x]), reverse=True)
+    
+    for key in dic_sort:
+        dic[key].sort(key=lambda x: plays[x], reverse=True)
+        for i in dic[key][:2]:
+            answer.append(i)
+    return answer
+  
+############
+def solution(genres, plays):
+    answer = []
+    dic = {}
+    album_list = []
+    for i in range(len(genres)):
+        dic[genres[i]] = dic.get(genres[i], 0) + plays[i]
+        album_list.append(album(genres[i], plays[i], i))
+
+    dic = sorted(dic.items(), key=lambda dic:dic[1], reverse=True)
+    album_list = sorted(album_list, reverse=True)
+
+
+
+    while len(dic) > 0:
+        play_genre = dic.pop(0)
+        print(play_genre)
+        cnt = 0;
+        for ab in album_list:
+            if play_genre[0] == ab.genre:
+                answer.append(ab.track)
+                cnt += 1
+            if cnt == 2:
+                break
+
+    return answer
+
+class album:
+    def __init__(self, genre, play, track):
+        self.genre = genre
+        self.play = play
+        self.track = track
+
+    def __lt__(self, other):
+        return self.play < other.play
+    def __le__(self, other):
+        return self.play <= other.play
+    def __gt__(self, other):
+        return self.play > other.play
+    def __ge__(self, other):
+        return self.play >= other.play
+    def __eq__(self, other):
+        return self.play == other.play
+    def __ne__(self, other):
+        return self.play != other.play
