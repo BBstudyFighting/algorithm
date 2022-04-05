d16547776a63e272f7e66c69f54861ab986d0c4f
wltn39 <wltn39@gmail.com>
2022-04-05 10:41:31 +0900

    add annotation

--- 07주차/jisu/algo_swea/5108_숫자추가.py
+++ 07주차/jisu/algo_swea/5108_숫자추가.py
@@ -1,7 +1,10 @@
 for tc in range(1, int(input()) + 1):
+    # 최초수열갯수 N, 추가숫자갯수 M, 출력할 인덱스 L
     N, M, L = map(int, input().split())
+    # 수열만들기
     array = list(map(int, input().split()))
     for _ in range(M):
         idx, num = map(int, input().split())
-        array.insert(idx,num)
+        # 인덱스와 숫자입력받고, insert로 지정한 위치에 넣기
+        array.insert(idx, num)
     print(f'#{tc} {array[L]}')
--- 07주차/jisu/sql_hack/the_count_functio.sql
+++ 07주차/jisu/sql_hack/the_count_functio.sql
@@ -1,6 +1,6 @@
 select 
-    count(*)
+    count(*) -- count 출력
 from 
     city
 where
-    population > 100000;
+    population > 100000; -- 조건 지정
