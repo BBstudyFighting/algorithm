33867dac17b588aecccc3dac947a2818ddcab4fc
ssu-yeon <a46448489@gmail.com>
2022-04-06 23:17:04 +0900

    suyeon_Weather Observation Station 11(sql)

---
+++ 07주차/suyeon/sql_hack/Weather Observation Station 11.sql
@@ -0,0 +1,6 @@
+'''
+Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels. Your result cannot contain duplicates.
+'''
+SELECT DISTINCT city
+FROM station
+WHERE city REGEXP '^[^aeiou]' OR city REGEXP '[^aeiou]$'
