4a03c65a545f098642af57127d18a1e1e6243a73
ssu-yeon <a46448489@gmail.com>
2022-04-07 23:15:41 +0900

    suyeon_Employee Names(sql)

---
+++ 07주차/suyeon/sql_hack/Employee Names.sql
@@ -0,0 +1,6 @@
+'''
+Write a query that prints a list of employee names (i.e.: the name attribute) from the Employee table in alphabetical order.
+'''
+SELECT name
+from Employee 
+Order by name asc ;
