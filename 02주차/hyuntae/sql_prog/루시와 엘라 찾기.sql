4b2dd51d141733020c8896585b84228b2dc6c705
Hyuntae Jung <adp_jht@naver.com>
2022-04-16 01:09:43 +0900

    hyuntae Sql_String&Date

---
+++ 02주차/hyuntae/sql_prog/루시와 엘라 찾기.sql
@@ -0,0 +1,5 @@
+-- 루시와 엘라 찾기
+SELECT ANIMAL_ID, NAME, SEX_UPON_INTAKE
+FROM ANIMAL_INS
+WHERE NAME IN ('Lucy', 'Ella', 'Pickle', 'Rogan', 'Sabrina', 'Mitty')
+ORDER BY ANIMAL_ID ASC;
---
+++ 02주차/hyuntae/sql_prog/중성화 여부 파악하기.sql
@@ -0,0 +1,10 @@
+-- 중성화 여부 파악하기 'CASE ~ WHEN ~ THEN ~ ELSE    END'
+SELECT ANIMAL_ID, NAME,
+CASE
+    WHEN SEX_UPON_INTAKE LIKE 'Neutered%' OR SEX_UPON_INTAKE LIKE 'Spayed%' 
+        THEN 'O'
+    ELSE
+        'X'
+END AS "중성화"
+FROM ANIMAL_INS
+ORDER BY ANIMAL_ID ASC;
