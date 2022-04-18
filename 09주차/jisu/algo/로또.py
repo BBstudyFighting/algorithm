f588c08c44559b415ee97346a439a96877c51000
wltn39 <wltn39@gmail.com>
2022-04-18 21:54:35 +0900

    modify

--- 09주차/jisu/algo/로또.py
+++ 09주차/jisu/algo/로또.py
@@ -1,6 +1,6 @@
 def solution(lottos, win_nums):
     cnt = len(set(lottos) & set(win_nums))  # 로또 맞힌 숫자 갯수
-    cnt_0 = lottos.count(0)  # 로또에서 낙서로 지워진 부분
+    zero = lottos.count(0)  # 로또에서 낙서로 지워진 부분
 
     # 0을 다 맞다고 봤을 때와 아닐때 값 구하기
     return [7-max(cnt+zero, 1), 7-max(cnt, 1)]
