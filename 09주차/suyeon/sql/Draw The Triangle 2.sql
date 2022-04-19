993d65ecec3ba6304e7acbccd43f408320e6c448
ssu-yeon <a46448489@gmail.com>
2022-04-19 21:52:01 +0900

    suyeon_Draw The Triangle 2(sql)

---
+++ 09주차/suyeon/sql/Draw The Triangle 2.sql
@@ -0,0 +1,13 @@
+"""
+P(R) represents a pattern drawn by Julia in R rows. The following pattern represents P(5):
+
+* 
+* * 
+* * * 
+* * * * 
+* * * * *
+Write a query to print the pattern P(20).
+"""
+SET @number = 0; 
+SELECT REPEAT('* ', @number := @number + 1) 
+FROM information_schema.tables limit 20;  
