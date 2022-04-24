9fc7ba80a5ce4d97789b1d2e1fb5b2f5b0083f41
ssu-yeon <a46448489@gmail.com>
2022-04-24 23:12:53 +0900

    suyeon_신규 아이디 추천(algo)

--- 09주차/suyeon/algo/신규 아이디 추천.py
+++ 09주차/suyeon/algo/신규 아이디 추천.py
@@ -6,6 +6,7 @@
 # 6단계 new_id의 길이가 16자 이상이면, new_id의 첫 15개의 문자를 제외한 나머지 문자들을 모두 제거합니다. 만약 제거 후 마침표(.)가 new_id의 끝에 위치한다면 끝에 위치한 마침표(.) 문자를 제거합니다.
 # 7단계 new_id의 길이가 2자 이하라면, new_id의 마지막 문자를 new_id의 길이가 3이 될 때까지 반복해서 끝에 붙입니다.
 
+# 1번째 방법
 def solution(new_id):
     # 1단계
     new_id = new_id.lower()
@@ -33,7 +34,7 @@ def solution(new_id):
     return answer
 
 
-
+# 2번째 방법
 import re
 
 def solution(new_id):
@@ -47,7 +48,7 @@ def solution(new_id):
     st = st if len(st) > 2 else st + "".join([st[-1] for i in range(3-len(st))])
     return st
 
-
+# 3번째 방법
 def solution(new_id):
     answer = ''
     # 1
