031a5030cde351f029e4d7e5859f13f37e0e1702
ssu-yeon <a46448489@gmail.com>
2022-04-29 22:04:07 +0900

    suyeon_Weather Observation Station 1(sql복습)

---
+++ 10주차/SUYEON/SQL/Weather Observation Station 1.sql
@@ -0,0 +1,9 @@
+'''
+Query a list of CITY and STATE from the STATION table.
+The STATION table is described as follows:
+
+where LAT_N is the northern latitude and LONG_W is the western longitude.
+'''
+
+SELECT CITY , STATE
+from STATION
