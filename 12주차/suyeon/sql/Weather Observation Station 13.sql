cee4484c2fd2b32c85d4c3ba080a5c6b86f2082c
ssu-yeon <a46448489@gmail.com>
2022-05-10 18:53:23 +0900

    suyeon_Weather Observation Station 13(sql복습)

---
+++ 12주차/suyeon/sql/Weather Observation Station 13.sql
@@ -0,0 +1,6 @@
+'''
+Query the sum of Northern Latitudes (LAT_N) from STATION having values greater than  38.7880 and less than 137.2345. Truncate your answer to  4 decimal places.
+'''
+SELECT Truncate(sum(LAT_N), 4)
+FROM STATION
+WHERE LAT_N > 38.7880 and LAT_N < 137.2345
