ef42ff3d10fab5976eed60d874d1a6b69d7d40e8
ssu-yeon <a46448489@gmail.com>
2022-06-12 18:57:24 +0900

    suyeon_DATETIME에서 DATE로 형 변환(sql복습 수정)

--- 16주차/SUYEON/SQL/DATETIME에서 DATE로 형 변환.sql
+++ 16주차/SUYEON/SQL/DATETIME에서 DATE로 형 변환.sql
@@ -1,3 +1,13 @@
-SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, "%Y-%M-%M")
+SELECT ANIMAL_ID, NAME, DATE_FORMAT(DATETIME, "%Y-%M-%M") #DATE_FORMAT(값, '원하는 형태')
 FROM ANIMAL_INS
 WHERE ANIMAL_ID
+
+# 자주쓰는 date 형식
+-- %Y : 4자리 연도
+-- %y : 2자리 연도
+-- %m : 2자리 월
+-- %d : 2자리 일
+-- %H : 24시간 형식 시(00-23)
+-- %h : 12시간 형식 시(01-12)
+-- %i : 2자리 분(0-59)
+-- %S, %s : 2자리 초(0-59)
