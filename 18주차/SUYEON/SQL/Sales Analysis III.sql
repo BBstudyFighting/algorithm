5513a5fcbec08fd27366ce56098dc64eb41a31db
ssu-yeon <a46448489@gmail.com>
2022-08-10 10:44:06 +0900

    Sales Analysis III

---
+++ 18주차/SUYEON/SQL/Sales Analysis III.sql
@@ -0,0 +1,5 @@
+SELECT p.product_id, p.product_name
+FROM Product p, Sales s
+WHERE p.product_id = s.product_id
+GROUP BY s.product_id
+HAVING MIN(s.sale_date) >= '2019-01-01' AND MAX(s.sale_date) <= '2019-03-31'
