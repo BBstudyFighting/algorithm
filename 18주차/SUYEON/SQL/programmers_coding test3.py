d54d5b40095d3ffc3634d2dc9275e39ac8c51b5c
ssu-yeon <a46448489@gmail.com>
2022-09-18 21:25:29 +0900

    programmers_coding test3

---
+++ 18주차/SUYEON/SQL/programmers_coding test3.py
@@ -0,0 +1,22 @@
+# 전화번호 목록
+
+def solution(phone_book):
+    phone_book = sorted(phone_book)
+    for p1, p2 in zip (phone_book, phone_book[1:]):
+        if p2.startswith(p1):
+            return False
+    return True
+
+
+def solution(phone_book):
+    answer = True
+    hash_map = {}
+    for phone_number in phone_book:
+        hash_map[phone_number] = 1
+    for phone_number in phone_book:
+        temp = ""
+        for number in phone_number:
+            temp += number
+            if temp in hash_map and temp != phone_number:
+                return False
+    return answer
