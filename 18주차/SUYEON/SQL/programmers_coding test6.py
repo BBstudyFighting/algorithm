44d09eb6e31c971dc33e0aca8d10cedc98dede8d
ssu-yeon <a46448489@gmail.com>
2022-09-21 21:08:24 +0900

    programmers_coding test6(수정)

--- 18주차/SUYEON/SQL/programmers_coding test6.py
+++ 18주차/SUYEON/SQL/programmers_coding test6.py
@@ -4,6 +4,7 @@ def solution(array, commands):
     
     for i in commands:
         ary = array[i[0]-1: i[1]]    # 문제에서 주어진 크기만큼 자르기
+        # new_array = array[command[0]:command[1]]  이렇게 짠다면 command[2]:command[5] 이렇게 나오게 됩니다. => 3번째부터 5번째수까지 잘리게됨. n번째부터 자른다고 하면 n-1번째 인덱스부터 잘라주어야합니다.
         ary.sort()    # sort 함수로 정렬
         answer.append(ary[i[2]-1])    # k 번째 값 집어넣기
         
