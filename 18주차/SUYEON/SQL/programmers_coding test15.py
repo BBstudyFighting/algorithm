9f93f8a4965bb03f7d00563cb9cf6705af26a5be
ssu-yeon <a46448489@gmail.com>
2022-09-30 23:21:49 +0900

    programmers_coding test15

---
+++ 18주차/SUYEON/SQL/programmers_coding test15.py
@@ -0,0 +1,20 @@
+# 모음사전
+from itertools import product
+
+def solution(word):
+    words = []
+    for i in range(1, 6):
+        for c in product(['A', 'E', 'I', 'O', 'U'], repeat=i):
+            words.append(''.join(list(c)))
+
+    words.sort()
+    return words.index(word) + 1
+
+
+
+
+
+
+from itertools import product
+
+solution = lambda word: sorted(["".join(c) for i in range(5) for c in product("AEIOU", repeat=i+1)]).index(word) + 1
