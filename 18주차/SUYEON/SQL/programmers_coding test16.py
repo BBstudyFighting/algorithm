4f1eeee5582af4114c91e5a9f4a87a07ce7b777e
ssu-yeon <a46448489@gmail.com>
2022-10-01 22:48:37 +0900

    programmers_coding test16

---
+++ 18주차/SUYEON/SQL/programmers_coding test16.py
@@ -0,0 +1,70 @@
+# 타겟 넘버
+# [재귀]
+def solution(numbers, target):
+    if not numbers and target == 0 :
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:], target-numbers[0]) + solution(numbers[1:], target+numbers[0])
+
+
+
+from itertools import product
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
+
+
+# deque를 이용한 BFS 풀이
+from collections import deque
+def solution(numbers, target):
+    answer = 0
+    queue = deque()
+    n = len(numbers)
+    queue.append([numbers[0],0])
+    queue.append([-1*numbers[0],0])
+    while queue:
+        temp, idx = queue.popleft()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+
+
+## stack을 이용한 dfs여도 마찬가지! 
+def solution(numbers, target):
+    answer = 0
+    queue = [[numbers[0],0], [-1*numbers[0],0]]
+    n = len(numbers)
+    while queue:
+        temp, idx = queue.pop()
+        idx += 1
+        if idx < n:
+            queue.append([temp+numbers[idx], idx])
+            queue.append([temp-numbers[idx], idx])
+        else:
+            if temp == target:
+                answer += 1
+    return answer
+
+# 재귀함수를 이용한 DFS 풀이
+def solution(numbers, target):
+    n = len(numbers)
+    answer = 0
+    def dfs(idx, result):
+        if idx == n:
+            if result == target:
+                nonlocal answer
+                answer += 1
+            return
+        else:
+            dfs(idx+1, result+numbers[idx])
+            dfs(idx+1, result-numbers[idx])
+    dfs(0,0)
+    return answer
