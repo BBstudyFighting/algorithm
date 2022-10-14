61023cc0aae80865b0417b3ea2e0aad6dab815c0
ssu-yeon <a46448489@gmail.com>
2022-10-14 23:08:08 +0900

    programmers_coding test29

---
+++ 18주차/SUYEON/SQL/programmers_coding test29.py
@@ -0,0 +1,27 @@
+def solution(priorities, location):
+      answer = 0
+  from collections import deque
+
+  d = deque([(v,i) for i,v in enumerate(priorities)])
+
+  while len(d):
+      item = d.popleft()
+      if d and max(d)[0] > item[0]:
+          d.append(item)
+      else:
+          answer += 1
+          if item[1] == location:
+              break
+  return answer
+##########
+def solution(priorities, location):
+    queue =  [(i,p) for i,p in enumerate(priorities)]
+    answer = 0
+    while True:
+        cur = queue.pop(0)
+        if any(cur[1] < q[1] for q in queue):
+            queue.append(cur)
+        else:
+            answer += 1
+            if cur[0] == location:
+                return answer
