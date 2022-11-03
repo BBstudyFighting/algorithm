2e2244f86bdc9fe667d6e2714a049bdd45f25ed0
ssu-yeon <a46448489@gmail.com>
2022-11-03 20:32:21 +0900

    programmers_coding test48

---
+++ 18주차/SUYEON/SQL/programmers_coding test48.py
@@ -0,0 +1,27 @@
+# 몫 구하기
+def solution(num1, num2):
+    answer = num1//num2
+    return answer
+
+# 두 수의 차
+def solution(num1, num2):
+    answer = num1 - num2
+    return answer
+
+# 숫자 비교하기
+def solution(num1, num2):
+    if num1 != num2 : 
+        return -1
+    else : 
+        return 1
+    
+    
+def solution(num1, num2):
+    if num1 == num2 : 
+        return 1
+    else : 
+        return -1
+    
+# 두 수의 곱
+def solution(num1, num2):
+    return num1 * num2
