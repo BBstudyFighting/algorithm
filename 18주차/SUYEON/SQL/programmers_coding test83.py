1c5b561bcec1975c3c80a8ef1354cb6c0490c987
ssu-yeon <a46448489@gmail.com>
2022-12-09 23:51:12 +0900

    programmers_coding test83

---
+++ 18주차/SUYEON/SQL/programmers_coding test83.py
@@ -0,0 +1,27 @@
+# 안전지대
+def solution(board):
+    n = len(board)
+    danger = set()
+    for i, row in enumerate(board):
+        for j, x in enumerate(row):
+            if not x:
+                continue
+            danger.update((i+di, j+dj) for di in [-1,0,1] for dj in [-1, 0, 1])
+    return n*n - sum(0 <= i < n and 0 <= j < n for i, j in danger)
+
+
+def solution(board):
+    answer = 0
+
+    for col in range(len(board)):
+        for row in range(len(board[col])):
+            if board[row][col] == 1:
+                for j in range(max(col-1,0),min(col+2,len(board))):
+                    for i in range(max(row-1,0),min(row+2,len(board))):
+                        if board[i][j] == 1:
+                            continue
+                        board[i][j] = -1
+    for i in board:
+        answer += i.count(0)
+
+    return answer
