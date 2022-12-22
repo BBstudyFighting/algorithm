01ffbe03c3eb1a004d18c19f621944134ff77741
ssu-yeon <a46448489@gmail.com>
2022-12-22 18:25:00 +0900

    programmers_coding test96

---
+++ 18주차/SUYEON/SQL/programmers_coding test96.py
@@ -0,0 +1,12 @@
+# 평행
+from itertools import combinations
+
+def solution(dots):
+    a = []
+    for (x1,y1),(x2,y2) in combinations(dots,2):
+        a.append((y2-y1,x2-x1))
+
+    for (x1,y1),(x2,y2) in combinations(a,2):
+        if x1*y2==x2*y1:
+            return 1
+    return 0
